ac9fcad21dc76cad5e4f979666c88624f4e04a18
emilygc51 <73391555+emilygc51@users.noreply.github.com>
2022-02-08 16:07:55 -0600

    Completed Project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# project1
-This is my first project for Art151
+# Growing Shapes
+
+Utilize circles, squares, and triangles to create modern art with a randomized color palette and an alterable growing rate.
+
+Instructions: Click anywhere to start growing a shape. Default is a circle, but you can change the shape by clicking 1 for circle, 2 for square, and 3 for triangle on your keyboard. You can also increase the rate of your shape growing with the up arrow key or decrease the rate with the down arrow key.
--- sketch.js
+++ sketch.js
@@ -1,21 +1,64 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  window.alert('Click anywhere to start growing a shape. Default is a circle, but you can change the shape by clicking 1 for circle, 2 for square, and 3 for triangle on your keyboard. You can also increase the rate of your shape growing with the up arrow key or decrease the rate with the down arrow key.');
   tempx = windowWidth/2;
   tempy = windowHeight/2;
+  background(0);
+  counter = 1;
+  increment = 0.5;
+  clickCounter = 0;
+  tempShape = 'circle';
+  shape = 'none';
 }
 
-// let tempx = windowWidth/2;
-// let tempy = windowHeight/2;
-// let speed = 4;
-
 function draw() {
-  background(255, 204, 229);
-  circle(tempx, tempy, random(1,50));
-  tempx = tempx+4;
-
+    strokeWeight(.5);
+    stroke(250);
+    fill(random(255), random(255), random(255), random(255))
+    if (shape == 'circle') {
+        circle(tempx, tempy, counter);
+    }
+    else if (shape == 'square') {
+        rect(tempx, tempy, counter);
+    }
+    else if (shape == 'triangle') {
+        triangle(tempx, tempy, tempx+(counter/2), tempy+counter, tempx+counter, tempy);
+    }
+    else if (shape == 'none') {
+        ;
+    }
+    counter = counter + increment;
 }
 
 function mouseClicked() {
-  tempx = mouseX;
-  tempy = mouseY;
+//  clickCounter++;
+//  if (clickCounter == 2 || clickCounter % 2 == 0) {
+//    tempx = -1000;
+//    tempy = -1000;
+//    shape = 'none';
+//  }
+//  else {
+    tempx = mouseX;
+    tempy = mouseY;
+    shape = tempShape;
+    counter = 0;
+//  }
+}
+
+function keyPressed() {
+    if (key === '1') {
+        tempShape = 'circle';
+    }
+    else if (key === '2') {
+        tempShape = 'square';
+    }
+    else if (key === '3') {
+        tempShape = 'triangle';
+    }
+    else if (keyCode == UP_ARROW) {
+        increment = increment + 0.1;
+    }
+    else if (keyCode == DOWN_ARROW && increment >= 0.1) {
+        increment = increment - 0.1;
+    }
 }
